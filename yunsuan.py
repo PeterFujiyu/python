5e902d0a85ff2af8979daad7d6eae5d2cd51d7c4
PeterFujiyu <88922024+PeterFujiyu@users.noreply.github.com>
2021-08-14 17:28:23 +0800

    更新运算算法

--- yunsuan.py
+++ yunsuan.py
@@ -5,7 +5,7 @@ def is_number(s):
         return True
     except ValueError:
         pass
- 
+
     try:
         import unicodedata
         unicodedata.numeric(s)
@@ -22,34 +22,27 @@ if not is_number(op1):
 
 op = input('请输入运算符（必须是+，-，*，/）：')
 if not ((op == '+') or (op == '-') or (op == '*') or (op == '/')):
-   print('抱歉，您运算符输入错误') 
+   print('抱歉，您运算符输入错误')
    sys.exit(1)
 op2 = input('请输入第二个运算数（必须是数字）：')
 if not is_number(op2):
     print('抱歉，您第二个运算数输入错误')
     sys.exit(1)
-    
-op1 = float(op1)
-op2 = float(op2)
-      
+
+#op1 = float(op1)
+#op2 = float(op2)
+
 
 if (op == '+'):
-    print(op1 + op2)   
+    print(op1 + '+' + op2 + '=' + str(float(op1) + float(op2)))
 elif (op == '-'):
-    print(op1 - op2) 
+    print(op1 + '-' + op2 + '=' + str(float(op1) - float(op2)))
 elif (op == '*'):
-    print(op1 * op2) 
+    print(op1 + '*' + op2 + '=' + str(float(op1) * float(op2)))
 elif (op == '/' ):
     if (op2 == 0):
         print('抱歉，您输入错误(除数不能为0)')
         sys.exit(1)
     else:
-        print(op1 / op2)
-    
-
-
-
-
-
-
+        print(op1 + '/' + op2 + '=' + str(float(op1) / float(op2)))
 
